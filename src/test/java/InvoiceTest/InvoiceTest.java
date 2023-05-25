4b365c30c9b6fbffb90a40bbafbc460e33eea97e
SkazuHD <voda0110@gmail.com>
2023-05-25 20:41:53 +0200

    TODO NEW TESTS

--- src/test/java/InvoiceTest/InvoiceTest.java
+++ src/test/java/InvoiceTest/InvoiceTest.java
@@ -1,12 +1,9 @@
 package InvoiceTest;
 
-import org.eshop.entities.Customer;
-import org.eshop.entities.User;
 import org.eshop.exceptions.LoginFailed;
 import org.eshop.exceptions.NotInStockException;
 import org.eshop.exceptions.ProductNotFound;
 import org.eshop.exceptions.UserExistsException;
-import org.eshop.shop.Shop;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -15,12 +12,6 @@ public class InvoiceTest {
     @Disabled
     @Test
     void testInvoice() throws UserExistsException, LoginFailed, ProductNotFound, NotInStockException {
-        Shop shop = new Shop();
-        User c = shop.loginUser("test", "test");
-        shop.addProductToCart("test0", 5, (Customer) c);
-        shop.addProductToCart("test1", 5, (Customer) c);
-        shop.addProductToCart("test2", 5, (Customer) c);
-
 
     }
 }
