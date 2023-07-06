16b961eb3b4ae5e67a1ddecba928716ba42f2626
Kim <kroch@stud.hs-bremen.de>
2023-07-06 13:45:29 +0200

    New Methods

--- src/main/java/org/eshop/ui/frames/CheckOutFrame.java
+++ src/main/java/org/eshop/ui/frames/CheckOutFrame.java
@@ -1,15 +1,31 @@
 package org.eshop.ui.frames;
 
 import org.eshop.entities.Customer;
+import org.eshop.exceptions.CheckoutFailed;
 import org.eshop.shop.Shop;
+import org.eshop.ui.models.CartModel;
+import org.eshop.ui.models.productEmployeeModel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CheckOutFrame extends JFrame {
-    private final javax.swing.JPanel jPanel1 = new javax.swing.JPanel();
+    private JPanel mainPanel;
 
-    public CheckOutFrame(Customer c, Shop shop){
-        //jPanel1
+
+    private final Shop server;
+
+    public CheckOutFrame(Customer c, Shop shop) {
+        server = shop;
+        buildUI();
 
     }
+    private void buildUI() {
+
+        mainPanel = new JPanel();
+        this.add(mainPanel);
+        mainPanel.setBackground(new Color(40));
+        pack();
+        this.setVisible(true);}
 }
+
